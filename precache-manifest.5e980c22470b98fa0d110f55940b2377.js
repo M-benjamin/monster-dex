2cf72c26a4bdc5497c241037b5f0b718d55a51d8
M-benjamin <mballabenjamin@yahoo.fr>
2020-07-05 17:16:47 +0200

    Updates

---
+++ precache-manifest.5e980c22470b98fa0d110f55940b2377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d422e4ac58a09d014fb14001741eab",
+    "url": "/monster-dex/index.html"
+  },
+  {
+    "revision": "cdd13e867aed1a5c0ce2",
+    "url": "/monster-dex/static/css/main.1c783d0c.chunk.css"
+  },
+  {
+    "revision": "831e9600a6310362c5a6",
+    "url": "/monster-dex/static/js/2.3d5c5e20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-dex/static/js/2.3d5c5e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd13e867aed1a5c0ce2",
+    "url": "/monster-dex/static/js/main.b78ce541.chunk.js"
+  },
+  {
+    "revision": "49fea22d4735864c7834",
+    "url": "/monster-dex/static/js/runtime-main.26054f30.js"
+  }
+]);
